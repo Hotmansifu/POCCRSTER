d80c260ad20ef5faf3182330afbd8ef28c298685
HotmanSifu <l_andghuladze3@cu.edu.ge>
2023-02-09 00:54:23 +0400

    added gui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
-import datetime
+import tkinter as tk
+from tkinter import messagebox
 
 def get_ip_address(hostname):
     try:
@@ -18,67 +19,91 @@ def is_open(host, port):
     except:
         return "closed or filtered"
 
-choice = input("Do you want to scan a website (w) or an IP address (i)? ")
+def start_scan():
+    host = None
+    if choice.get() == 1:
+        hostname = hostname_entry.get()
+        host = get_ip_address(hostname)
+        if host is None:
+            messagebox.showerror("Error", "Unable to resolve hostname.")
+            return
+    elif choice.get() == 2:
+        host = ip_entry.get()
 
-if choice == 'w':
-    hostname = input("Enter a website hostname: ")
-    host = get_ip_address(hostname)
-    if host is None:
-        print("Unable to resolve hostname.")
-    else:
-        port_choice = input("Do you want to scan all possible ports (a) or specific ones (s)? ")
-        if port_choice == 'a':
-            print("Scanning host", host, "...")
-            for port in range(1, 65535):
-                result = is_open(host, port)
-                if result == "open":
-                    print("Port", port, "is", result)
-                else:
-                    print("Port", port, "is", result)
-            print("Scan completed on", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        elif port_choice == 's':
-            ports = input("Enter a list of ports separated by comma: ")
-            if ports.strip() == '':
-                print("No ports entered.")
-            else:
-                ports = [int(port) for port in ports.split(',')]
-                print("Scanning host", host, "...")
-                for port in ports:
-                    result = is_open(host, port)
-                    if result == "open":
-                        print("Port", port, "is", result)
-                    else:
-                        print("Port", port, "is", result)
-                print("Scan completed on", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        else:
-            print("Invalid choice.")
-
-elif choice == 'i':
-    host = input("Enter an IP address: ")
-    port_choice = input("Do you want to scan all possible ports (a) or specific ones (s)? ")
-    if port_choice == 'a':
-        print("Scanning host", host, "...")
+    if port_choice.get() == 1:
         for port in range(1, 65535):
             result = is_open(host, port)
             if result == "open":
-                print("Port", port, "is", result)
+                result_list.insert(tk.END, "Port {} is {}".format(port, result))
             else:
-                print("Port", port, "is", result)
-    elif port_choice == 's':
-        ports = input("Enter a list of ports separated by comma: ")
-        if ports.strip() == '':
-            print("No ports entered.")
+                result_list.insert(tk.END, "Port {} is {}".format(port, result))
+    elif port_choice.get() == 2:
+        ports = ports_entry.get().split(',')
+        if ports[0].strip() == '':
+            messagebox.showerror("Error", "No ports entered.")
+            return
         else:
-            ports = [int(port) for port in ports.split(',')]
-            print("Scanning host", host, "...")
+            ports = [int(port) for port in ports]
             for port in ports:
                 result = is_open(host, port)
                 if result == "open":
-                    print("Port", port, "is", result)
+                    result_list.insert(tk.END, "Port {} is {}".format(port, result))
                 else:
-                    print("Port", port, "is", result)
-    else:
-        print("Invalid choice.")
+                    result_list.insert(tk.END, "Port {} is {}".format(port, result))
 
-else:
-    print("Invalid choice.")
+root = tk.Tk()
+root.title("Port Scanner")
+
+
+background_image = tk.PhotoImage(file="background.gif")
+background_label = tk.Label(root, image=background_image)
+background_label.pack(fill="both", expand=True)
+
+choice = tk.IntVar()
+
+hostname_frame = tk.Frame(root)
+hostname_frame.pack(pady=10)
+
+tk.Radiobutton(hostname_frame, text="Website hostname", variable=choice, value=1).pack(side="left")
+hostname_entry = tk.Entry(hostname_frame)
+hostname_entry.pack(side="left", fill="x", expand=True)
+
+ip_frame = tk.Frame(root)
+ip_frame.pack(pady=10)
+
+tk.Radiobutton(ip_frame, text="IP address", variable=choice, value=2).pack(side="left")
+ip_entry = tk.Entry(ip_frame)
+ip_entry.pack(side="left", fill="x", expand=True)
+
+port_choice = tk.IntVar()
+ports_description = tk.Label(root, text="Enter specific ports to be scanned (separated by commas):")
+ports_description.pack(pady=10)
+
+
+ports_frame = tk.Frame(root)
+ports_frame.pack(pady=10)
+
+ports_frame_label = tk.Frame(root)
+ports_frame_label.pack(pady=10)
+
+
+tk.Radiobutton(ports_frame_label, text="Scan specific ports", variable=port_choice, value=2).pack(side="left")
+
+ports_entry = tk.Entry(ports_frame)
+ports_entry.pack(side="left", fill="x",expand=True)
+
+start_button = tk.Button(root, text="Start Scan", command=start_scan)
+start_button.pack(pady=10)
+
+result_frame = tk.Frame(root)
+result_frame.pack(fill="both", expand=True)
+
+result_scrollbar = tk.Scrollbar(result_frame)
+result_scrollbar.pack(side="right", fill="y")
+
+result_list = tk.Listbox(result_frame, yscrollcommand=result_scrollbar.set)
+result_list.pack(side="left", fill="both", expand=True)
+
+result_scrollbar.config(command=result_list.yview)
+
+root.mainloop()
